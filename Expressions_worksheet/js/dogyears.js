e1f7f77f413552130139225b16a5a95209bbc089
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-13 17:14:44 -0600

    Setup Expressions_worksheet folder
    Created .html and dog years.js

---
+++ Expressions_worksheet/js/dogyears.js
@@ -0,0 +1,34 @@
+/*
+Kyle Szymanski
+WPF 1408 Section 02
+Dog years
+8-13-14
+ */
+
+// Spraky the dog is x in human years.
+
+// Find out how old Sparky the dog is in dog years.
+
+var humanYears = prompt("what is the age of sparky in human years?");
+
+console.log(humanYears)
+
+// How old is sparky in human years?
+
+// Dog years are 7 times the age of a human.
+
+// Multiply 7 by the age of the human.
+
+var dogYears= humanYears * 7;
+
+// console log out response
+
+console.log(dogYears);
+
+// This is how old sparky is.
+
+// Print out output of sparkys age.
+
+console.log(" Sparky is " + humanYears + " human years old which is " + dogYears + " in dog years ");
+
+
