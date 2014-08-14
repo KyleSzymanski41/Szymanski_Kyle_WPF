1769bfa20c9ba9280265f853d47969a283cbabb1
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-13 20:25:55 -0600

    slices of pie part 2
    working on finding result

--- Expressions_worksheet/js/SliceofPie2.js
+++ Expressions_worksheet/js/SliceofPie2.js
@@ -1,3 +1,37 @@
-/**
- * Created by kyleszymanski on 8/13/14.
+/*
+ Kyle Szymanski
+ WPF 1408 Section 02
+ Slices of Pie part 2
+ 8-13-14
  */
+
+//Sparky the hosts dog gets to eat the left over pizza. How many slices will he get assuming the people eat whole slices?
+
+var people = 10;
+
+var pizzaOrdered = 4;
+
+var slicesperPizza = 8;
+
+// To find out how many slices there are we need to multiply the number of pizza to the number of slices per pizza.
+
+var totalSlices = pizzaOrdered * slicesperPizza
+
+console.log(totalSlices);
+
+// Now that we know the number of slice we have overall, we need to divide that by the number of people.
+
+var slices = totalSlices / people;
+
+console.log(totalSlices / people);
+
+// This would give us our answer for slices per person.
+
+console.log(slices);
+
+//Assuming everyone only eats whole slices
+
+
+
+
+
