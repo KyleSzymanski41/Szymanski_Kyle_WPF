5b0643333cb3740d305efbd662eb99dfaee582ce
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-13 23:42:37 -0600

    created expressions_personal

---
+++ Expressions_Personal/js/canreturn.js
@@ -0,0 +1,41 @@
+/*
+ Kyle Szymanski
+ 8-13-14
+ Assignment: Expressions
+ WPF - WDD 144 Section 02
+ */
+
+// You and some friends return cans to a recycling center for extra money. How much money will each person receive?
+
+// Givens
+// Ask user how many friends helped return cans.
+
+var people = prompt("How many friends help you return cans?");
+
+// Console log out response.
+
+console.log(people);
+
+// Ask user what the return value for each can is in cents.
+
+var value = prompt("What is the return value for each can worth in cents? Use decimals.");
+
+//Console log out response.
+
+console.log(value);
+
+// Ask user how many cans they returned.
+
+var cans = prompt("How many cans did you return?");
+
+// Console log out response.
+
+console.log(cans);
+
+//Ask the user what the price for returning the can is.
+
+var price = prompt("How much does the fee cost to return cans?");
+
+//Console log out response.
+
+console.log(price);
