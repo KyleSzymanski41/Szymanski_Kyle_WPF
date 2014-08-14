065d2d4dfd962434aa8917e00e5ac1d2c9331502
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-14 01:46:49 -0600

    lap time
    creating an array by asking lap time 1-4

--- Expressions_Wacky/js/laptimes.js
+++ Expressions_Wacky/js/laptimes.js
@@ -8,4 +8,42 @@
 // The user wants to know how he did at track day.
 // Calculate the average of his lap times using an array.
 // Determine his how fast he was going at that time. Use miles per hour.
+// Array
 
+var lapTimes = [lap1 , lap2 , lap3 ,lap4];
+
+// Ask the user their name.
+
+var name = prompt("what is your name?");
+
+alert("Hello " + name);
+
+// Ask the user for their lap times one at a time for an array.
+
+var lap1 = prompt("What is lap time number 1?");
+
+// console.log out response.
+
+console.log(lap1);
+
+// Ask them for 2nd lap time
+
+var lap2 = prompt("What is your 2nd lap time?");
+
+// console.log out response.
+
+console.log(lap2);
+
+// Ask them for their 3rd lap time.
+
+var lap3 = prompt("What is your 3rd lap time?");
+
+// console.log out response
+
+console.log(lap3);
+
+// Ask them for their final lap time.
+
+var lap4 = prompt("What is your final lap time?");
+
+// console.log out response.
