3143f59c31922a253e0a2725a678752a80629ad7
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-14 10:49:53 -0600

    Stated problem and how to solve

--- Expressions_Industry/js/platform.js
+++ Expressions_Industry/js/platform.js
@@ -4,3 +4,17 @@
  Expressions
  8-14-13
  */
+
+
+// The user is building a rectangular platform for his work station.
+// Use L x W x H to find the volume
+// Then use 2LW + 2LH + 2WH to find the surface area.
+// Inform the user of his volume and surface area
+
+// Ask the user for the length of the platform in feet.
+
+var length = prompt("What is the length of the platform in feet?");
+
+// console.log out response.
+
+console.log(length);
