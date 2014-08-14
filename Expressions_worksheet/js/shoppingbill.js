aaa901d0969f8ad0612837a9779d57d4e5fe26fd
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-13 21:32:42 -0600

    Started Average shopping bills

---
+++ Expressions_worksheet/js/shoppingbill.js
@@ -0,0 +1,18 @@
+/*
+ Kyle Szymanski
+ WPF 1408 Section 02
+ Average Shopping Bill
+ 8-13-14
+ */
+
+//array
+var shoppingBills = [287.63 , 426.24 , 127.99 , 333.11 , 220.00]; // 0,1,2,3,4
+
+console.log(shoppingBills)
+
+
+//You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the past five weeks.
+
+// Create an expression that will use the items in the array to calculate the average amount spent on groceries.
+
+shoppingBills
