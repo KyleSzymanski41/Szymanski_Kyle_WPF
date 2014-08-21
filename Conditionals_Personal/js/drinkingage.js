965c21e9a3d7456611bc385736145fe70a410b9a
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-20 21:58:50 -0600

    Created Conditional personal index and js file

---
+++ Conditionals_Personal/js/drinkingage.js
@@ -0,0 +1,30 @@
+/*
+Kyle Szymanski
+WPF- WDD 148 - section 02
+8-20-14
+Assignment - Conditionals
+ */
+
+// The user is looking to go out for an alcoholic beverage with his friends, find out if he can.
+
+// Ask the user his or her name.
+
+var name = prompt(" What is your name?");
+
+// Say hello to the user!
+
+alert(" Hello " + name + " nice to meet you!");
+
+// Find out if the user is able to join his friends for a alcoholic beverage
+
+var drinkingAge = 24;
+
+// console.log out drinking age
+
+console.log(drinkingAge);
+
+// Ask the user to input their age.
+
+var age = prompt(" Please tell me your age.");
+
+// 
