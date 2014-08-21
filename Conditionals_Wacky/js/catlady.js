18c32761bae0df3ee52dab7ad3f7feef037f6b46
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-21 00:23:40 -0600

    created first variables

--- Conditionals_Wacky/js/catlady.js
+++ Conditionals_Wacky/js/catlady.js
@@ -7,10 +7,23 @@
 
 var maxCats = 3;
 
+// Ask the owner how many cats they have.
+
 var userCats = prompt("How many cats do you have?");
 
-var houseHold = prompt("How many people are in your home?");
 
 if(userCats === ""){ // If the user did not answer, prompt them again.
     userCats = prompt(" What is the number have cats you own?");
 }
+
+// Otherwise continue.
+
+// ask the user how many people are in the house hold.
+
+var houseHold = prompt("How many people are in your home?");
+
+// console.log out response.
+
+console.log(houseHold);
+
+
