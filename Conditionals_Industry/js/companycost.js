075dc2df7c1b322aaf5b128588597a6578c184fb
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-21 01:26:40 -0600

    asked user for electric cost
    using an if statement asked user for electric bill

--- Conditionals_Industry/js/companycost.js
+++ Conditionals_Industry/js/companycost.js
@@ -11,4 +11,16 @@
 
 // Ask the user how much his fixed costs are in dollars.
 
+// Ask the user for their electric bill.
+
+var electric = prompt("How much is your electric bill in dollars?");
+
+if(electric === ""){// If the user does not enter a value, prompt again.
+    electric = prompt("In dollar amount how much is your electric bill?");
+}
+
+// Otherwise validate prompt.
+
+console.log(electric);
+
 
