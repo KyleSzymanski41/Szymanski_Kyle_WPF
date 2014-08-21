5c24773fc972cd4d949a5fbcca3c574e43921576
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-21 01:25:53 -0600

    started stating variables

---
+++ Conditionals_Industry/js/companycost.js
@@ -0,0 +1,14 @@
+/**
+ Kyle Szymanski
+ 8/21/14.
+ Assignment-Conditionals
+ WPF-WDD 144- section 02
+ */
+
+// The user is the president of a development company. The company runs on a specific budget of 5,000 a week.
+// The user needs to find out if his total cost is less than, greater than, or equal to his budget before his government loan kicks in.
+// The users fixed cost is based on his electric bill, rent, and insurance costs.
+
+// Ask the user how much his fixed costs are in dollars.
+
+
