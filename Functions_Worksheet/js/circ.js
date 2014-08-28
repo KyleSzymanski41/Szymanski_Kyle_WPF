50d8c62651e31715541445a60c97b430987dfddc
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-27 21:32:19 -0600

    started function and return

--- Functions_Worksheet/js/circ.js
+++ Functions_Worksheet/js/circ.js
@@ -3,3 +3,35 @@
  * Circumference of a circle.
  * WPF - WDD 144 - Functions worksheet
  */
+
+// A user is looking for the circumference of a circle.
+
+// Find the radius of the circle.
+
+var r = prompt(" What is the radius of the circle?");
+
+// console.log out information
+
+console.log(parseInt(r));
+
+var d = 2 * parseInt(r);
+
+console.log(parseInt(d));
+
+var p = 3.14;
+
+var circumference = calculateCircumference(d, p);
+
+
+function calculateCircumference(d, p){
+
+    var circumCircle = d * p;
+
+    return circumCircle;
+
+}
+
+console.log(circumference);
+
+
+
