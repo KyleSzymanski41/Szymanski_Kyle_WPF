244310477c19b0a43fd525e7fd729535c8ac9ec8
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-28 00:39:35 -0600

    asked for bundles and pallets

--- Functions_Industry/js/js.js
+++ Functions_Industry/js/js.js
@@ -6,6 +6,24 @@
  */
 
 // The Shipping manager for a phone book plant is concerned he may be short on the product being shipped.
-// He knows they have extra in storage just in case.
+// He knows they have extra in storage just in case which would meet the order.
 // Help the manager determine if he has to use his stored supply.
+
+// Ask the user how many books are bundled per wrap.
+
+var booksperBundle = prompt("How many books are bundled per wrap?");
+
+// console.log out response
+
+console.log(booksperBundle);
+
+// Ask the user how many pallets they have shipped.
+
+var shipPallets = prompt("How many pallets have shipped?");
+
+// console.log out response.
+
+console.log(shipPallets);
+
+//
 
