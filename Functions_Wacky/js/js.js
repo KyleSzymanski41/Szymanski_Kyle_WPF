be9fc48b507af0f8833453f71aa8f1114b591ca7
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-27 22:54:52 -0600

    started golfing by asking for par

---
+++ Functions_Wacky/js/js.js
@@ -0,0 +1,15 @@
+/**
+ kyleszymanski
+ 8/27/14.
+ WPF-WDD 144
+ Assignment - Functions
+ */
+
+// The user is wondering how his golf game is doing. Are they getting better, worse or even?
+
+// Ask the user for the following, Par of the hole, and shots taken.
+
+var par = prompt("What is par for the hole?");
+
+console.log(par);
+
