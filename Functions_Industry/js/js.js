01dba8be663ce04a8b6d804014a68cf94e7d4f98
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-28 00:31:33 -0600

    determined a problem to solve
    created a problem concerning shippments

--- Functions_Industry/js/js.js
+++ Functions_Industry/js/js.js
@@ -4,3 +4,8 @@
  * WPF-WDD 144
  * Assignment - Functions
  */
+
+// The Shipping manager for a phone book plant is concerned he may be short on the product being shipped.
+// He knows they have extra in storage just in case.
+// Help the manager determine if he has to use his stored supply.
+
