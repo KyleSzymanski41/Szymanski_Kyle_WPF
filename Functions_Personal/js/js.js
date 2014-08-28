87737235602b365e947f9306a6ae64341344ea76
KyleSzymanski41 <kyleszymanski@Kyles-MacBook-Pro.local>
2014-08-28 01:41:15 -0600

    added variables people, sky cost and video cost

--- Functions_Personal/js/js.js
+++ Functions_Personal/js/js.js
@@ -1,3 +1,33 @@
 /**
- * Created by kyleszymanski on 8/28/14.
+ * Kyle Szymanski
+ * WPF-WDD 144
+ * Assignment- Functions
+ * 8/28/14.
  */
+
+// The user and their friends are planning a skydiving adventure, and get video proof of this.
+// Determine if each person brought enough money to go.
+
+// Ask the user how many people are going.
+
+var people = prompt(" How many people are going skydiving?");
+
+// console.log out response.
+
+console.log(people);
+
+// Ask the user what the total cost for skydiving is as a group.
+
+var skyTotal = prompt(" How much is it for the group to skydive total?");
+
+// console.log out response
+
+console.log(parseInt(skyTotal));
+
+// Ask the user how much the total video price would be for the group.
+
+var videoPrice = prompt("How much is the total video cost for the group?");
+
+// console.log out response.
+
+console.log(parseInt(videoPrice));
